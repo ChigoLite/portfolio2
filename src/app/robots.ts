6684db73d4e4522a00cd6f8e73eb08453bd9bdca
Chigolite <nelsonlite5065@gmail.com>
2025-05-17 04:51:44 +0100

    update seo

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://ninetalenttech.com/sitemap.xml",
+    sitemap: "https://chigolite.vercel.app/sitemap.xml",
   }
 }
